0bec7ee49f9735b62a625c46afc8b154af986143
arrosid3008 <muhammadarrosid3008@gmail.com>
2024-02-27 21:47:54 +0700

    Baru

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const App = () => (
-    <div className="bg-primary w-full overflow-hidden">
-      <h1>Selamat Datang di Bank kami</h1>
-    </div>
+  <div className="bg-primary w-full overflow-hidden">
+    <h1>Selamat Datang</h1>
+  </div>
 );
 
 export default App
